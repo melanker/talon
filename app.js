6ffd2d73c8d0ddc47e37132f48259e0b56ddb451
melanker <melankeril@gmail.com>
2014-04-29 21:58:10 +0300

    3

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(express.static(__dirname + '/public'));
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
