c3a4842a59c62945fc8c60ed1b26bde0f26af573
melanker <melankeril@gmail.com>
2014-04-29 21:15:29 +0300

    new

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+webApp = angular.module('webApp', ['ui.bootstrap']);
+
+webApp.controller('CarouselDemoCtrl', function($scope) {
+    var BASE_URL = 'https://dl.dropboxusercontent.com/u/25108583/header';
+    $scope.myInterval = 5000;
+    $scope.slides = [{image: BASE_URL + '1.png'}, {image: BASE_URL + '2.png'}, {image:BASE_URL + '3.png'}];
+
+});
